9837003176d1941638058df7c517324aa4aa2ddc
RajasiDeshpande <rajasideshpande04@gmail.com>
2025-05-08 02:01:10 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const express=require("express");
+const app=express()
+const mongoose=require("mongoose");
+const port=8080;
+const MONGO_URL="mongodb://127.0.0.1/wanderlust";
+const Listing=require("./models/listing.js");
+const path=require("path");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.set("views",path.join(__dirname,"views/listings"));
+app.use(express.urlencoded({extended:true}));
+app.listen(port,()=>{
+    console.log(`server is listening through ${port}`);
+})
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+// app.get("/testListing",async(req,res)=>{
+//     let sampleListing=new Listing({
+//         title:"My new villa",
+//         description:"By the beach",
+//         price:1200,
+//         location:"Calangute, Goa",
+//         country:"India",
+//     })
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("sucessful entry");
+// })
+main()
+.then(()=>{
+    console.log("connected to db");
+})
+.catch((err)=>{
+    console.log(err);
+})
+//Index Route
+app.get("/listings",async(req,res)=>{
+    const allListings=await Listing.find({});
+    res.render("index.ejs",{allListings});
+})
+
+//New Route
+app.get("/listings/new",(req,res)=>{
+    res.render("new.ejs");
+})
+
+//Show Route
+app.get("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    const detail= await Listing.findById(id);
+    res.render("show.ejs",{detail});
+})
+
+//Create Route
+app.post("/listings", async(req,res)=>{
+    const newListing=new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings")
+});
+
+app.get("/",(req,res)=>{
+    res.send("Hi, I am root");
+})
---
+++ models/listing.js
@@ -0,0 +1,19 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+const listingSchema=new Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:String,
+    image:{
+        type:String,
+        default:"https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=",
+        set: (v)=>v===""?"https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=":v,
+    },
+    price:Number,
+    location:String,
+    country:String,
+});
+const Listing=mongoose.model("Listing",listingSchema);
+module.exports=Listing;
